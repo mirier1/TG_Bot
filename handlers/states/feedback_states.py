109fb744507e1ee1627994e924f8a47be3a50399
Tigr6446 <ilnaz051107@gmail.com>
2026-03-28 15:47:43 +0400

    ReturnToMicroFix

--- handlers/states/feedback_states.py
+++
@@ -1,7 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class FeedbackStates(StatesGroup):
-    usefulness = State()
-    interest = State()
-    clarity = State()
-    comment = State()
