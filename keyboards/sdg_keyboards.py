b9be327b65f34d29318930fc11b9d289c3d557aa
Tigr6446 <ilnaz051107@gmail.com>
2026-05-21 08:10:25 +0400

    FIX

--- keyboards/sdg_keyboards.py
+++ keyboards/sdg_keyboards.py
@@ -13,7 +13,7 @@ def get_sdg_list_kb():
             callback_data=f"sdg_{num}"
         ))
     
-    builder.adjust(2)
+    builder.adjust(1)
     builder.row(InlineKeyboardButton(
         text="◀️ Назад в меню",
         callback_data="back_to_main_menu"
